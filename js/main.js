e8101294b6936d05e1c734ebebc12e4fdf7df8fe
Lyam Edjekouane <lyamedjekouane@MacBook-Pro-de-Lyam.local>
2025-10-22 14:59:32 +0200

    Add Section1 background

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/** GSAP Plugins */
+gsap.registerPlugin(ScrollTrigger, Observer, ScrollToPlugin, Draggable, MotionPathPlugin);
+
+
+/** GSAP Animation - S1 -  */
+gsap.to("#algerie",{
+    x: 100,
+    rotation: 90,
+    duration: 1,
+})
+
+gsap.to
