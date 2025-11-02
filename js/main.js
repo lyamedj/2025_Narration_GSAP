4a5bf9b9ea147ad8f2e200112bc1cb7b0553b225
Lyam Edjekouane <lyamedjekouane@MacBook-Pro-de-Lyam.local>
2025-11-02 16:58:45 +0100

    Add Section4 Quiz and Responsive

--- js/main.js
+++ js/main.js
@@ -109,3 +109,131 @@ gsap.utils.toArray('.clip').forEach((clip, i) => {
         ease: "power3.out"
     })
 })
+
+
+/** QUIZ */
+
+// Séléction 
+
+const intro = document.querySelector(".introQuizActive");
+const startBtn = document.getElementById("startQuiz");
+const questions = Array.from(document.querySelectorAll(".quiz .question"));
+const resultatBloc = document.querySelector(".quizResultat");
+const resultatText = document.getElementById("resultat");
+const retryBtn = document.getElementById("retryQuiz");
+
+let current = 0;
+let score = 0;
+let locked = false;
+
+// Apparition de la section 
+
+gsap.from(".quiz .titreSection", {
+    scrollTrigger: { trigger: ".quiz", start: "top 85%"},
+    opacity: 0,
+    y: 30,
+    duration: 0.6,
+    ease: "power2.out",
+});
+
+// Masquer tout
+
+function toutCacher(){
+    intro.classList.remove("active");
+    resultatBloc.classList.remove("active");
+    questions.forEach((q) => q.classList.remove("active"))
+}
+
+// Afficher intro
+
+function showIntro() {
+    toutCacher();
+    intro.classList.add("active");
+    gsap.fromTo(intro, {opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 0.6 })
+}
+
+// Afficher question
+
+function showQuestion(i) {
+    toutCacher();
+    const q = questions[i]; // le i va gerer l'affichage de la question dans le quiz puis la const va me permettre de recuperer la question 
+    if (!q) return showResultat();
+
+    q.classList.add("active");
+    locked = false;
+
+    gsap.fromTo(q, { opacity: 0, y: 24 }, { opacity: 1, y: 0, duration: 0.6, ease: "power2.out" });
+
+    q.querySelectorAll(".option img").forEach((img) => {
+        img.onclick = () => {
+            if (locked) return;
+            locked = true;
+
+        const isCorrect = img.getAttribute("data-correct") === "vrai";
+
+            if (isCorrect) {
+                score++; // si c'est correct cela ajoutera donc 1 point et mettras une petite animation verte ou rouge si bon ou non 
+                gsap.to(img, { borderColor: "#22c55e", boxShadow: "0 0 20px rgba(34,197,94,0.3)", duration: 0.3 });
+            } else {
+                gsap.to(img, { borderColor: "#ef4444", boxShadow: "0 0 20px rgba(239,68,68,0.3)", duration: 0.3 });
+                const good = q.querySelector('.option img[data-correct="vrai"]');
+            if (good) gsap.to(good, { borderColor: "#22c55e", duration: 0.25, delay: 0.1 });
+            }
+
+            setTimeout(() => { // c'est avec ce document que j'ai reussi a mettre en place le timeout (https://developer.mozilla.org/en-US/docs/Web/API/Window/setTimeout)
+                gsap.to(q, {
+                opacity: 0,
+                y: -20,
+                duration: 0.35,
+                ease: "power1.out",
+                onComplete: () => { // cela va permettre d'incrémenter le compteur (current), et donc affichera une nouvelle question 
+                    current++;
+                    showQuestion(current);
+                },
+            });
+        }, 700); // cela est donc le nombre qui signifie que la transition ne se declenchera qu'apres 0.7s
+    };
+  });
+}
+
+// Afficher le résultat final
+function showResultat() {
+    toutCacher();
+    resultatBloc.classList.add("active");
+
+    let message = "";
+    if (score === 5) {
+        message = " Parfait ! Tu es un vrai fan de Future ! ⭐";
+    } else if (score >= 3) {
+        message = " Bien joué ! Tu connais assez bien. 🔥";
+    } else {
+        message = "Tu as encore beaucoup a découvrir...😅";
+    }
+    resultatText.textContent = message + " (" + score + "/5)";
+
+    gsap.fromTo(resultatBloc, { opacity: 0, scale: 0.92 }, { opacity: 1, scale: 1, duration: 0.8, ease: "power3.out" });
+}
+
+// Rejouer
+
+retryBtn.addEventListener("click", () => {
+    current = 0;
+    score = 0;
+    showIntro();
+    document.querySelectorAll(".option img").forEach((img) => {
+        img.style.borderColor = "transparent";
+        img.style.boxShadow = "0 6px 18px rgba(0, 0, 0, 0.1)";
+  });
+});
+
+// Lancer le quiz
+
+startBtn.addEventListener("click", () => {
+    current = 0;
+    score = 0;
+    showQuestion(current);
+});
+
+// Démarrage initial
+
+showIntro();
